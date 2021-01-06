f11e86948e960f495cc04cd05526192d4d979330
Pratik Gorade <pratikgorade0@gmail.com>
2021-01-06 10:53:23 +0530

    parsed env args
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    let action: String = match env::args().nth(1) {
+        Some(action) => action,
+        None => {
+            println!("Please specify the action. Exiting...");
+            process::exit(0);
+        }
+    };
+    let item: String = match env::args().nth(2) {
+        Some(item) => item,
+        None => {
+            println!("Please specify the item. Exiting...");
+            process::exit(0)
+        }
+    };
+
+    println!("{} {}", action, item);
 }
