3384e9df2077e77a238e0d45ebd0d09a8a9108f9
Pratik Gorade <pratikgorade0@gmail.com>
2021-01-06 11:41:39 +0530

    created struct ToDo and insert and save function
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
-use std::{env, process};
+use std::{env, process, collections, fs, io};
+
+struct ToDo {
+    map: collections::HashMap<String, bool>,
+}
+
+impl ToDo {
+    fn insert(&mut self, key: String) {
+        // insert new item in map with default value as true
+        self.map.insert(key, true);
+    }
+
+    fn save(self) -> Result<(), io::Error> {
+        let mut content = String::new();
+        for (k, v) in self.map {
+            let record = format!("{}\t{}\n", k, v);
+            content.push_str(&record);
+        }
+        let mut db_path: String = match env::home_dir() {
+            Some(path) => path.into_os_string().into_string().unwrap(),
+            None => "".to_string(),
+        };
+        db_path.push_str("/.todo");
+        fs::write(db_path, content)
+    }
+}
 
 fn main() {
     let action: String = match env::args().nth(1) {
@@ -15,6 +40,4 @@ fn main() {
             process::exit(0)
         }
     };
-
-    println!("{} {}", action, item);
 }
