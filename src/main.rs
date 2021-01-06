f29926f2f5292bf5b23099a34307dd3926c3390b
Pratik Gorade <pratikgorade0@gmail.com>
2021-01-06 16:14:30 +0530

    added get db_path function to get the database file path created function called new in impl of ToDo struct to avoid overwriting the map on every action
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::{env, process, collections, fs, io};
+use std::{env, process, collections, fs, io, path, io::{Read}, str::{FromStr}};
+
+fn get_db_path() -> path::PathBuf {
+    env::home_dir().unwrap().join(path::Path::new(".todo"))
+}
 
 struct ToDo {
     map: collections::HashMap<String, bool>,
@@ -16,12 +20,24 @@ impl ToDo {
             let record = format!("{}\t{}\n", k, v);
             content.push_str(&record);
         }
-        let mut db_path: String = match env::home_dir() {
-            Some(path) => path.into_os_string().into_string().unwrap(),
-            None => "".to_string(),
-        };
-        db_path.push_str("/.todo");
-        fs::write(db_path, content)
+        fs::write(get_db_path(), content)
+    }
+
+    fn new() -> Result<ToDo, io::Error> {
+       let mut f = fs::OpenOptions::new()
+           .write(true)
+           .create(true)
+           .read(true)
+           .open(get_db_path())?;
+        let mut content = String::new();
+        f.read_to_string(&mut content)?;
+        let map: collections::HashMap<String, bool> = content
+            .lines()
+            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
+            .map(|v| (v[0], v[1]))
+            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
+            .collect();
+        Ok(ToDo { map })
     }
 }
 
@@ -40,10 +56,8 @@ fn main() {
             process::exit(0)
         }
     };
-    
-    let mut todo = ToDo {
-        map: collections::HashMap::new(),
-    };
+
+    let mut todo = ToDo::new().unwrap();
 
     if action ==  "add" {
         todo.insert(item);
